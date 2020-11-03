5c22ed3dbb1fd7502de7d087e38e79b71c9edc5a
Balazzzska <balazzska92@gmail.com>
2020-11-03 23:16:04 +0100

    initial commit

---
+++ movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class movement : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float speed = 2.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasAuthority)
+            return;
+
+        var move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            move += Vector3.left;// * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move += Vector3.right;// * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            move += Vector3.up;// * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move += Vector3.down;
+        }
+
+        //var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
